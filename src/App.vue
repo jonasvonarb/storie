b203bc910e75591a747f134149e8b0299b079855
jonasvonarb <mail@jonasvonarb.ch>
2024-09-20 15:52:36 +0200

    added api calls to dato CMS

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,18 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
 import HelloWorld from './components/HelloWorld.vue'
+
+import { useApiStore } from '@/stores'
+
+const apiStore = useApiStore()
+
+apiStore.call('tags')
+apiStore.call('projekte')
 </script>
 
 <template>
   <header>
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
     <div class="wrapper">
       <HelloWorld msg="You did it!" />
       <HelloWorld msg="You did it 2!" />
---
+++ src/stores/index.js
@@ -0,0 +1,93 @@
+import { ref } from 'vue'
+import { defineStore } from 'pinia'
+
+export const useApiStore = defineStore('apiCalls', () => {
+  const responses = ref({})
+  const lottie = ref(0)
+
+  const token = 'd0355b48bdc3c40d895b71991ec35e'
+
+  const call = (id) => {
+    if (!responses.value[id]) {
+      fetch('https://graphql.datocms.com/', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          Accept: 'application/json',
+          Authorization: `Bearer ${token}`
+        },
+        body: JSON.stringify({
+          query: querrys[id]
+        })
+      })
+        .then((res) => res.json())
+        .then((res) => {
+          let data = [res.data]
+          data.map((items) => {
+            Object.keys(items).map((item) => (responses.value[item] = items?.[item]))
+          })
+          console.log(responses.value)
+        })
+        .catch((error) => {
+          console.log(error)
+        })
+    }
+  }
+  const readJSON = (link) => {
+    if (!lottie.value) {
+      fetch(link)
+        .then((res) => res.json())
+        .then((res) => {
+          if (!lottie.value) {
+            lottie.value = res
+          }
+        })
+        .catch((error) => {
+          console.log(error)
+        })
+    }
+  }
+
+  const getVideo = (url) => {
+    fetch(url, {
+      method: 'GET'
+    })
+      .then((res) => res.json())
+      .then((res) => {
+        console.log(res)
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+  }
+
+  const querrys = {
+    projekte: `
+    {
+      projekte {
+        _modelApiKey
+        id 
+      }
+    }`,
+    tags: `
+    {
+      allTagClients {
+        id
+        _modelApiKey
+        label
+      }
+       allTagProjectSorts {
+        id
+        _modelApiKey
+        label
+      }
+      allTagsProjectAreas {
+        id
+        _modelApiKey
+        label
+      }
+    }`
+  }
+
+  return { responses, call, lottie, readJSON, getVideo }
+})
