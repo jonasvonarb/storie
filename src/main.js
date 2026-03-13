c77791cbddbe7b72e68ce927a151372409ebe15d
jonasvonarb <jonas@azt.ch>
2026-03-13 09:20:45 +0100

    fixed Vulnerability alerts for vite, rollup and unhead

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import './assets/main.styl'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-import { createHead } from '@unhead/vue'
+import { createHead } from '@unhead/vue/client'
 
 import App from './App.vue'
 import router from './router'
